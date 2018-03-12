d20a425f72ed894c32560303be93dacdf5ee5ba8
quelledanielle <quelledanielle@users.noreply.github.com>
2018-03-12 16:06:29 -0400

    Convert l values to grayscale string

--- process_gif.py
+++ process_gif.py
@@ -1,10 +1,24 @@
 ''' parse an animated gif for thermal printing '''
-from printer import ThermalPrinter
+# from printer import ThermalPrinter
 from PIL import Image
 import sys
 
-printer = ThermalPrinter(serialport='/dev/ttyAMA0')
+GRAYSCALE_CHARS = ['  ','░░','▒▒','▓▓']
+
+# printer = ThermalPrinter(serialport='/dev/ttyAMA0')
 gif = Image.open(sys.argv[1])
+
+def l_to_char(l):
+    bucket = 255 / 3
+    return GRAYSCALE_CHARS[int(l / bucket)]
+
+def to_string(chars):
+    data = ""
+    for idx, c in enumerate(chars):
+        if idx % base_width * 2 == 0:
+            data += '\n'
+        data += c
+    return data
 
 # separate out panels and process each one
 frame = 0
@@ -14,23 +28,23 @@ while frame < 10:
         gif.seek(frame)
 
         # resize image to printer width
-        base_width = 384
+        base_width = 16
         height = int(gif.size[1] * (base_width / float(gif.size[0])))
         converted = gif.resize((base_width, height), Image.ANTIALIAS)
 
         # convert the image to black and white
-        converted = converted.convert('1')
+        converted = converted.convert('L')
+        values = list(converted.getdata())
+        chars = list(map(l_to_char, values))
 
-        # convert to a matrix of pixels
-        data = list(converted.getdata())
-        w, h = converted.size
+        print(to_string(chars))
 
         # import os
         # converted.save(os.getcwd() + '/output/%d.bmp' % frame)
 
         # print each frame
-        printer.print_bitmap(data, w, h, True)
-        printer.linefeed()
+        # printer.print_bitmap(data, w, h, True)
+        # printer.linefeed()
 
         frame += 1
     except EOFError:
