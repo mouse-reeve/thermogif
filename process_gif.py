ebc5500e7a16d7e7dda82ee9d47546ec77b88570
Mouse Reeve <mousereeve@riseup.net>
2018-03-12 15:04:03 -0400

    Reize image and expect printer library

--- process_gif.py
+++ process_gif.py
@@ -1,20 +1,37 @@
 ''' parse an animated gif for thermal printing '''
-import os
+from printer import ThermalPrinter
 from PIL import Image
 import sys
 
+printer = ThermalPrinter(serialport='dev/ttyAMA0')
 gif = Image.open(sys.argv[1])
 
 # separate out panels and process each one
 frame = 0
 while frame < 10:
     try:
+        # get the current frame
         gif.seek(frame)
-        # convert the image to 4 shades of gray
-        converted = gif.convert('1')
 
-        # save each frame
-        converted.save('%s/output/%d.bmp' % (os.getcwd(), frame))
+        # resize image to printer width
+        base_width = 384
+        height = int(gif.size[1] * (base_width / float(gif.size[0])))
+        converted = gif.resize((base_width, height), Image.ANTIALIAS)
+
+        # convert the image to black and white
+        converted = converted.convert('1')
+
+        # convert to a matrix of pixels
+        data = list(converted.getdata())
+        w, h = converted.size
+
+        # import os
+        # converted.save(os.getcwd() + '/output/%d.bmp' % frame)
+
+        # print each frame
+        printer.print_bitmap(data, w, h, True)
+        printer.linefeed()
+
         frame += 1
     except EOFError:
         # found end gif
