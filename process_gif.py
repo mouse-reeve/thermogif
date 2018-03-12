39ad1cdb32e58d604bfc69c8f722c6f90cd42a29
Mouse Reeve <mousereeve@riseup.net>
2018-03-12 17:13:52 -0400

    Remember bitmaps????

--- process_gif.py
+++ process_gif.py
@@ -3,22 +3,8 @@ from printer import ThermalPrinter
 from PIL import Image
 import sys
 
-GRAYSCALE_CHARS = ['  ', '\xB0\xB0','\xB1\xB1','\xB2\xB2']
-
 printer = ThermalPrinter(serialport='/dev/ttyAMA0')
 gif = Image.open(sys.argv[1])
-
-def l_to_char(l):
-    bucket = 255 / 3
-    return GRAYSCALE_CHARS[int(l / bucket)]
-
-def to_string(chars):
-    data = ""
-    for idx, c in enumerate(chars):
-        if idx % base_width * 2 == 0:
-            data += '\n'
-        data += c
-    return data
 
 # separate out panels and process each one
 frame = 0
@@ -28,17 +14,19 @@ while frame < 10:
         gif.seek(frame)
 
         # resize image to printer width
-        base_width = 20
+        base_width = 384
         height = int(gif.size[1] * (base_width / float(gif.size[0])))
         converted = gif.resize((base_width, height), Image.ANTIALIAS)
 
         # convert the image to black and white
-        converted = converted.convert('L')
-        values = list(converted.getdata())
-        chars = list(map(l_to_char, values))
+        converted = converted.convert('1', dither=3)
+        data = list(converted.getdata())
+        w, h = converted.size
 
-        printer.font_b()
-        printer.print_text(to_string(chars))
+#        import os
+#        converted.save(os.getcwd() + '/output/%d.bmp' % frame)
+
+        printer.print_bitmap(data, w, h, True)
         printer.linefeed()
         printer.linefeed()
         printer.linefeed()
